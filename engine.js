8a69ba181f20ce16ed04e5c6948f0136667ad198
Skyyler Siejko <skyyconnected@gmail.com>
2017-11-30 23:17:01 -0800

    first commit

---
+++ engine.js
@@ -0,0 +1,231 @@
+var canvas = document.getElementById("canvas");  
+var ctx = canvas.getContext("2d");
+var width = canvas.width; 
+var height = canvas.height;  
+var frameRate = 1/80; // Seconds
+var frameDelay = frameRate * 1000; // ms       
+var loopTimer = false;   
+       
+class Constant{   
+    constructor(){
+        this.drag = 0.47; //0.47
+        this.density = 1.22;
+        this.gravity = 9.8;       
+    }
+}
+
+
+class Vector { 
+	constructor(x, y, z, direction) { 
+		this.y = y;
+		this.x = x;
+		this.z = z;
+		this.magnitude = Math.abs(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2));
+		this.direction = direction;  
+		this.values = [];
+		this.frequency = 1;
+	}
+
+	dotProductAngle(vector, theta) {
+		return this.magnitude * vector.magnitude * Math.cos(theta);
+	}  
+
+	dotProduct(vector) {
+		return (this.x * vector.x + this.y * vector.y + this.z * vector.z);
+	}
+
+	twoDrotate(theta, i, j) {
+		var iRot = this.values[i] * Math.cos(theta) - this.values[j] * Math.sin(theta);
+		var jRot = this.values[i] * Math.sin(theta) + this.values[j] * Math.cos(theta);
+		return [iRot, jRot];
+	}
+
+
+	threeD_Rotoate_X(theta, i, j, k) {
+		var jRot = this.values[j] * Math.cos(theta) - this.values[k] * Math.sin(theta);
+		var kRot = this.values[j] * Math.sin(theta) + this.values[k] * Math.cos(theta);
+		return [i, jRot, kRot];
+	}
+
+	threeD_Rotate_Y(theta, i, j, k) {
+		var iRot = this.values[i] * Math.cos(theta) - this.values[k] * Math.sin(theta);
+		var kRot = -this.value[i] * Math.cos(theta) + this.values[k] * Math.cos(theta);
+		return [iRot, j, kRot]
+	}
+
+	threeD_Rotoate_Z(theta, i, j, k) {
+		var iRot = this.values[i] * Math.cos(theta) - this.values[j] * Math.sin(theta);
+		var jRot = this.values[i] * Math.sin(theta) + this.values[j] * Math.cos(theta);
+		return [iRot, jRot, k];
+	}
+
+	isOrthogonal(vector, theta) {
+		if (this.dotProduct(vector, theta) == 0) {  
+			return true; 
+		} else {
+			return false;
+		}
+	}
+  
+  flip(){
+    if (this.direction == 1){
+      this.direction = 3 
+    } else if (this.direction == 3){
+      this.direction = 1
+    }else if (this.direction == 2){
+      this.direction = 4
+    }else if (this.direction == 4){
+      this.direction = 2
+    }
+     
+  }
+    
+}
+
+
+
+
+class Entity{
+    constructor(x,y,mass,radius,kx){           
+        this.x = x; 
+        this.y = y;  
+        this.mass = mass; 
+        this.radius = radius  
+        this.kx = kx;
+        this.velocity = {x:random(5), y:0}
+        this.vector = new Vector(this.x, this.y, 0, 1) ;       
+        this.area =  Math.PI * this.radius * this.radius / (10000); 
+        this.color = random(2)  
+    }
+  
+  draw(){       
+    
+    ctx.save();     
+    ctx.beginPath();
+    if (this.color == 1)   
+    ctx.fillStyle = "red"  
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+    ctx.fill();
+    ctx.translate(this.x, this.y);  
+    ctx.restore();         
+  }
+  
+  move(){
+    
+     this.x += acceleration(this)[0]*frameRate*100;
+     this.y += acceleration(this)[1]*frameRate*100;
+    
+  }
+}
+
+function random(num){
+  return Math.floor(Math.random()*num)
+}
+
+
+
+
+ function collision(objA,objB){
+    if (objA.x < objB.x + objB.radius &&
+    objA.x + objA.radius > objB.x &&
+    objA.y < objB.y + objB.radius &&
+    objA.radius + objA.y > objB.y) {
+      return true;
+    }else{
+      return false; 
+    }
+ }
+
+
+ function walls(obj){
+    if (obj.y > height - obj.radius) { 
+        obj.velocity.y *= obj.kx;  
+        obj.y = height - obj.radius;
+      
+    }
+    if (obj.x > width - obj.radius) {
+        obj.velocity.x *= obj.kx;
+        obj.x = width - obj.radius;
+      
+    }
+    if (obj.x < obj.radius) {
+        obj.velocity.x *= obj.kx;
+        obj.x = obj.radius;
+      
+    }
+ }
+
+
+function drag(obj){
+  var Fx = -0.5 * constant.drag * obj.area * constant.density * obj.velocity.x **3 /     Math.abs(obj.velocity.x);
+   var Fy = -0.5 * constant.drag * obj.area * constant.density * obj.velocity.y **3 / Math.abs(obj.velocity.y);
+  Fx = (isNaN(Fx) ? 0 : Fx);
+  Fy = (isNaN(Fy) ? 0 : Fy);
+  return [Fx, Fy];
+}
+   
+
+function acceleration(obj){
+  var ax = drag(obj)[0] / obj.mass;
+  var ay = constant.gravity + (drag(obj)[1] / obj.mass);  
+   obj.velocity.x += ax*frameRate;
+   obj.velocity.y += ay*frameRate;  
+   return [obj.velocity.x, obj.velocity.y]                                      
+}   
+ 
+
+
+function massCollision(a,b){          
+if(collision(a,b)) {                            
+    b.velocity.x = (((2*a.mass)/a.mass+b.mass)*b.velocity.x) + (((b.mass-a.mass)/(a.mass+b.mass))*b.velocity.x)*frameRate*100    
+    a.velocity.x = (((2*b.mass)/a.mass+b.mass)*a.velocity.x) + (((a.mass- b.mass)/(a.mass+b.mass))*a.velocity.x)*frameRate*100 
+       
+  } 
+}
+
+
+
+
+ function allPhysics(obj) {
+        var Fx = drag(obj)[0];
+        var Fy = drag(obj)[1];  
+           
+        Fx = (isNaN(Fx) ? 0 : Fx);
+        Fy = (isNaN(Fy) ? 0 : Fy); 
+ 
+        obj.x += acceleration(obj)[0]*frameRate*100;
+        obj.y += acceleration(obj)[1]*frameRate*100;   
+    
+        walls(obj) 
+}      
+
+
+function makeBalls(items, num){        
+  for(var i = 0; i < num; i++){
+    items.push(new Entity(1*i*random(15),1*i*random(6), random(2), random(40),-0.7) ) 
+  }
+  
+}
+
+
+function drawAll(items){
+  for( var i = 0; i < items.length; i++){
+    items[i].draw()
+    items[i].move()  
+    walls(items[i]) 
+    massCollision(items[i],items[i+1])      
+      
+  }
+}
+
+var sprites = [] 
+makeBalls(sprites, 4)  
+function update(){
+  ctx.clearRect(0,0,width,height);
+  drawAll(sprites) 
+ 
+} 
+
+
+var constant = new Constant()  
+var loopTimer = setInterval(update, frameDelay); 
